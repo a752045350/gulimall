b5c24229a92c550f1ae5b8e825b3d82434a9d11a
a752045350 <397815397@qq.com>
2020-05-15 17:43:22 +0800

    商城

---
+++ gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.gulimall.thirdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GulimallThirdPartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallThirdPartyApplication.class, args);
+    }
+
+}
